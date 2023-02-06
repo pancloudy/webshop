4ad4d2df81503fcec4d521c021f0b42bc97b9917
pancloudy <kelpatrik99@gmail.com>
2023-02-06 09:11:55 +0100

    products list update

--- resources/views/layouts/products-list.blade.php
+++ resources/views/layouts/products-list.blade.php
@@ -6,19 +6,39 @@
     float: left;
     width: 20%;
     padding: 5px;
+    margin: 5px;
+  border: 1px solid #ccc;
+    }
+    .row{
+        padding: 3px;
     }
     .row::after {
   content: "";
   clear: both;
   display: table;
 }
+ .pricegreen{
+color:greenyellow;
+ }
+ .pricered{
+color: red;
+text-decoration: line-through;
+ }
 </style>
 <br>
 @foreach ($product as $products )
 <div class=="row">
     <div class="column">
         {{ $products->name }}
-        <img src="{{ asset('images/' . $products->image) }}" style="width:100%" />
+        <img src="{{ asset('images/' . $products->image) }}" height="600" width="400"  />
+        @if ($products->original_price != $products->selling_price)
+            <div class="pricegreen">
+                {{ $products->selling_price}}
+            </div>
+            <div class="pricered">{{ $products->original_price }}</div>
+        @else
+        {{ $products->selling_price }}
+        @endif
     </div>
 
 </div>
