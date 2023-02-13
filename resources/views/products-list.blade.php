0a4d3ae94ceeebdf4b2c314ed06d3f364047d0b0
pancloudy <kelpatrik99@gmail.com>
2023-02-13 13:36:58 +0100

    products list update

--- resources/views/products-list.blade.php
+++ resources/views/products-list.blade.php
@@ -1,34 +1,58 @@
 @extends ('layouts.app')
 
 @section('content')
+
 <style>
     .card {
-      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        float: left;
+      text-align: center;
       transition: 0.3s;
-      width: 40%;
+      margin-right: 1rem;
+  margin-bottom: 1rem;
+      width:  22%;
+    height: auto;
+    object-fit: cover;
     }
-    
+    .card:after{
+        padding: 10px;
+    }
     .card:hover {
       box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     }
     
     .container {
-      padding: 2px 16px;
+      padding: 3px;
+      max-height: 500px;
     }
+    .pricegreen{
+color:greenyellow;
+ }
+ .pricered{
+color: red;
+text-decoration: line-through;
+ }
     </style>
-    </head>
     <body>
-    
-    <h2>Card</h2>
-    
+    @foreach ($product as $products )
+    <form action="{{ action('App\Http\Controllers\Admin\ProductController@details', $products->image) }}" enctype="multipart/form-data" >
     <div class="card">
-      <img src="img_avatar.png" alt="Avatar" style="width:100%">
+        <img src="{{ asset('images/' . $products->image) }}" style="height:200px"  />
       <div class="container">
-        <h4><b>John Doe</b></h4> 
-        <p>Architect & Engineer</p> 
+        <h4><b>{{ $products->name }}</b></h4> 
+        <p>@if ($products->original_price != $products->selling_price)
+            <div class="pricegreen">
+                <h4>{{ $products->selling_price}}</h4>
+            </div>
+            <div class="pricered"> <h4> {{ $products->original_price }} </h4></div>
+        @else
+        <h4>{{ $products->selling_price }}</h4>
+        @endif</p> 
+        <button type="submit">Megtekintés</button>
       </div>
     </div>
-    
+</form>
+    @endforeach
     </body>
 
 @endsection
